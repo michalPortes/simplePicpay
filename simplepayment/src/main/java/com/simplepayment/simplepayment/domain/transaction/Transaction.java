7413d6b156ea74aa128c5d2a5b5670cfc9d6fe51
michaelPortes <michael.santos@embraer.com.br>
2023-11-18 12:20:02 -0300

    feat: create Us

---
+++ simplepayment/src/main/java/com/simplepayment/simplepayment/domain/transaction/Transaction.java
@@ -0,0 +1,32 @@
+package com.simplepayment.simplepayment.domain.transaction;
+
+import com.simplepayment.simplepayment.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+}
